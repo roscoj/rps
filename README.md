5ced88e0fe31b24d72180d53fc148c249d15c7ec
Ross Jarrett <rossjarrett@gmail.com>
2020-05-22 17:18:11 +0100

    Final draft RPS

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 A javascript version of rock, paper, scissors
+
+Gameplay
+
+1. Player is prompted to choose one of rock, paper or scissors - this now needs some work in validating input
+2. Computer randomly selects one of rock, paper or scissors
+3. The combinations are compared
+4. A winner is announced
+5. Player is asked if wants to play again
+6. Game is replayed or ended dependent on player input
+
--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-console.log("This is working");
+function computerPlay() {
+    const options = ["rock", "paper", "scissors"];
+    return options[randomNum(3)];
+}
+
+function randomNum(upperLimit) {
+    return Math.floor(Math.random()*upperLimit);
+}
+
+function humanPlay() {
+    return promptHuman();
+}
+
+function promptHuman() {
+    return check(prompt("Please choose rock, paper or scissors...").toLowerCase());
+    // this needs altering to return differently!
+}
+
+function check(choice) {
+    if (choice == "rock") {
+        return choice;
+    } else if (choice == "paper") {
+        return choice;
+    } else if (choice == "scissors") {
+        return choice;
+    } else {
+        return promptHuman();
+    }
+}
+
+
+function calculateWinner(choice1, choice2) {
+    const combo = [choice1, choice2];
+    if (same(combo) == true) {
+        return "draw";
+    } else if (combo[0] == "rock" && combo[1] == "paper") {
+        return "computer";
+    } else if (combo[0] == "rock" && combo[1] == "scissors") {
+        return "human";
+    } else if (combo[0] == "paper" && combo[1] == "rock") {
+        return "human";
+    } else if (combo[0] == "paper" && combo[1] == "scissors") {
+        return "computer";
+    } else if (combo[0] == "scissors" && combo[1] == "rock") {
+        return "computer";
+    } else if (combo[0] == "scissors" && combo[1] == "paper") {
+        return "human";
+    }
+}
+
+function same(arr) {
+    if (arr[0] == arr[1]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function mainGameplay() {
+    const human = humanPlay();
+    const computer = computerPlay();
+    if (prompt(`Winner: ${calculateWinner(human, computer)}. Play again? 'y' for yes 'return' for no`) == "y") {
+        mainGameplay();
+    }
+}
+
+mainGameplay();
